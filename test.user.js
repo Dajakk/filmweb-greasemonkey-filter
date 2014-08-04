ff275270351afb2d5e6ad4b3001c72fd93159306
Dajakk <pilithewizard@gmail.com>
2014-08-04 15:52:46 +0200

    * greasemonkey plugin creation

---
+++ test.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name		Filmweb filter
+// @description	alamakota
+// @include		http://www.filmweb.pl/user/*/films/wanna-see
+// @grant     none
+// ==/UserScript==
+console.log('test 123');
+
+
